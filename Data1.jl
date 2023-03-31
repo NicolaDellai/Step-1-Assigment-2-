1128d5b5a1fbc37c2dd35234035b8a4c3b5a0a3e
NicolaDellai <s213518@dtu.dk>
2023-03-31 16:00:41 +0200

    S1

--- Data1.jl
+++ Data1.jl
@@ -4,11 +4,20 @@ using XLSX
 #Get the directory of the current file
 dir = @__DIR__
 
+
+#Hours
+h = ["h$i" for i =1:24]
+H = length(h)
 #Day-Ahead prices and Wind production data
 DAfilepath = joinpath(dir, "Data", "DAprices.xlsx")
 DA = XLSX.readdata(DAfilepath,"DK2","A2:T25")                    #€/MWh
+DA_s = 20
+DA_array = [DA[1:H, c] for c = 1:DA_s]
 Wfilepath = joinpath(dir, "Data", "WindProduction.xlsx")
-W = XLSX.readdata(Wfilepath,"Normalized","C2:L25")                        #MW
+W_RT = XLSX.readdata(Wfilepath,"Normalized","B2:K25")             #MW
+W_RT_s = 10
+W_RT_array = [W_RT[1:H, c] for c = 1:W_RT_s]
+W_Cap = 150 #MW
 
 #=
 Generate a series of 24 random binary (two-state) variables, e.g., using a bernoulli distribution, indicating
@@ -47,5 +56,39 @@ SB =[0 0 1;
      0 1 1; 
      1 0 1; 
      1 0 1]
+
+SB_s = 3
+SB_array = [SB[1:H, c] for c = 1:SB_s]
+
+#GENERATE THE SCENARIOS 
+SC_d = Dict()
+counter = 0
+
+struct SCENARIO
+    WP::Vector{Float64}
+    PR::Vector{Float64}
+    IM::Vector{Int64}
+end
+
+for w=1:W_RT_s
+    for p=1:DA_s
+        for i=1:SB_s
+            global counter += 1
+            SC_d[counter] = SCENARIO(W_RT_array[w], DA_array[p], SB_array[i])
+        end
+    end
+end
+
+S = W_RT_s*DA_s*SB_s
+
+f_WP = zeros(H,S)
+f_DA = zeros(H,S)
+f_IM = zeros(H,S)
+
+for s=1:S
+    f_WP[1:H,s] = SC_d[s].WP
+    f_DA[1:H,s] = SC_d[s].PR
+    f_IM[1:H,s] = SC_d[s].IM
+end
 
 
--- Step1_OnePrice.jl
+++ Step1_OnePrice.jl
@@ -4,9 +4,35 @@ using Gurobi
 include("Data1.jl")
 
 
-@variable(S1_OnePrice, p_DA[h=1:24] >= 0)
-@variable(S1_OnePrice, I[h=1:24,w=1:200])
+#Model
+S1_OnePrice = Model(Gurobi.Optimizer)
+
+#Consider only the first 200 scenarios
+S = 200
+PI = [1/S for i=1:S]
+
+#Variables
+@variable(S1_OnePrice, w_da[h=1:H] >= 0)
+@variable(S1_OnePrice, w_up[h=1:H,s=1:S] >= 0) #upward change in wind production
+@variable(S1_OnePrice, w_dw[h=1:H,s=1:S] >= 0) #downward change in wind production
+@variable(S1_OnePrice, im[h=1:H,s=1:S]) #change in wind power production in hour h and scenario s
 
 
-@objective(S1_OnePrice, sum(sum(p[w] * (DA[h,d]*p_DA[h] + 0.9))))
+#Objective function One Price Scheme
+@objective(S1_OnePrice, Max, 
+            sum( sum(PI[s]
+            * ((f_DA[h,s] * w_da[h]) 
+            + (0.9 * f_DA[h,s] * w_up[h,s])  
+            - (1.2 * f_DA[h,s] * w_dw[h,s])) 
+            for s=1:S) for h=1:H)
+            )
+
+#Constraints
+@constraint(S1_OnePrice, [h=1:H], 0 <= w_da[h] <= W_Cap) # Capacity constraint for the wind farm
+#@constraint(S1_OnePrice, [h=1:H,s=1:S], im[h,s] == f_WP[h,s] - w_da[h] ) #real time imbalance
+@constraint(S1_OnePrice, [h=1:H,s=1:S], im[h,s] == w_up[h,s] - w_dw[h,s]) #excess and deficit imbalance equal the real time imbalance
+
+#Solve
+Solution = optimize!(S1_OnePrice)
+println(Solution)
 
