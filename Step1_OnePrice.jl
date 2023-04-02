c98296f35ae6f7a6c8d5e125c21f059652df5cc6
NicolaDellai <s213518@dtu.dk>
2023-04-02 19:09:07 +0200

    w

--- Step1_OnePrice.jl
+++ Step1_OnePrice.jl
@@ -13,33 +13,56 @@ PI = [1/S for i=1:S]
 
 #Variables
 @variable(S1_OnePrice, w_da[h=1:H] >= 0) #day ahead hourly bidded production
-@variable(S1_OnePrice, w_im_ex[h=1:H,s=1:S]) #change in wind power production in hour h and scenario s when the system has power excess
-@variable(S1_OnePrice, w_up_ex[h=1:H,s=1:S] >= 0) #upward change in wind production when the system has power excess
-@variable(S1_OnePrice, w_dw_ex[h=1:H,s=1:S] >= 0) #downward change in wind production when the system has power excess
-@variable(S1_OnePrice, w_im_def[h=1:H,s=1:S]) #change in wind power production in hour h and scenario s when the system has power deficit
-@variable(S1_OnePrice, w_up_def[h=1:H,s=1:S] >= 0) #upward change in wind production when the system has power deficit
-@variable(S1_OnePrice, w_dw_def[h=1:H,s=1:S] >= 0) #downward change in wind production when the system has power deficit
-
-@variable(S1_OnePrice, w_pos[h=1:H,s=1:S])
+@variable(S1_OnePrice, w_im[h=1:H,s=1:S])
+@variable(S1_OnePrice, w_up[h=1:H,s=1:S] >= 0)
+@variable(S1_OnePrice, w_dw[h=1:H,s=1:S] >= 0)
 
 #Objective function One Price Scheme
 @objective(S1_OnePrice, Max, 
             sum( sum(PI[s]
             * ((f_DA[h,s] * w_da[h]) 
-            + f_SB[h,s] * ( - (0.9 * f_DA[h,s] * w_up_ex[h,s]) + (0.9 * f_DA[h,s] * w_dw_ex[h,s]) ) #active when the system has power excess
-            + abs((f_SB[h,s]-1)) * ( (1.2 * f_DA[h,s] * w_up_def[h,s]) - (1.2 * f_DA[h,s] * w_dw_def[h,s]) ) #active when the system has power deficit
+            + f_SB[h,s] * ( - (0.9 * f_DA[h,s] * w_up[h,s]) + (0.9 * f_DA[h,s] * w_dw[h,s]) ) #active when the system has power excess
+            + abs((f_SB[h,s]-1)) * ( (1.2 * f_DA[h,s] * w_up[h,s]) - (1.2 * f_DA[h,s] * w_dw[h,s]) ) #active when the system has power deficit
             ) 
             for s=1:S) for h=1:H)
             )
 
 #Constraints
 @constraint(S1_OnePrice, [h=1:H], 0 <= w_da[h] <= W_Cap) #capacity constraint for the wind farm
-@constraint(S1_OnePrice, [h=1:H,s=1:S], w_im_ex[h,s] == f_WP[h,s] - w_da[h] ) #real time imbalance
-@constraint(S1_OnePrice, [h=1:H,s=1:S], w_im_ex[h,s] == w_up_ex[h,s] - w_dw_ex[h,s] ) #excess and deficit imbalance equal the real time imbalance
-@constraint(S1_OnePrice, [h=1:H,s=1:S], w_im_def[h,s] == f_WP[h,s] - w_da[h] ) #real time imbalance
-@constraint(S1_OnePrice, [h=1:H,s=1:S], w_im_def[h,s] == w_up_def[h,s] - w_dw_def[h,s] ) #excess and deficit imbalance equal the real time imbalance
+@constraint(S1_OnePrice, [h=1:H,s=1:S], w_im[h,s] == f_WP[h,s] - w_da[h] ) #real time imbalance
+@constraint(S1_OnePrice, [h=1:H,s=1:S], w_im[h,s] == w_up[h,s] - w_dw[h,s] ) #excess and deficit imbalance equal the real time imbalance
 
 #Solve
 Solution = optimize!(S1_OnePrice)
 println(Solution)
+
+println("Profit under the One Price scheme: $(round.(objective_value(S1_TwoPrice)))\$")
+
+#Outputs
+W_DA = value.(w_da[:])
+W_IM = value.(w_im[:,:])
+W_UP = value.(w_up[:,:])
+W_DW = value.(w_dw[:,:])
+
+println("Hourly Wind Power Production Scheduled in the Day-Ahead Market:")
+for h=1:H
+    println("$(h-1)-$(h): $(round.(W_DA[h], digits = 2))MW")
+end
+println("")
+println("")
+
+println("---------------------------------------------------------------")
+println("")
+
+println("Data of each Scenario:")
+
+for s=1:S
+    println("")
+    println("Senario $s:")
+    println("F_WP\tDA_WP\tIMB")
+    for h=1:H
+    println("$(round(f_WP[h,s]))\t$(round(W_DA[h]))\t$(round(W_IM[h,s]))")
+    end
+end
+
 
