e57ad9373ec65d01e47461ab51802d2de9c0e45f
NicolaDellai <s213518@dtu.dk>
2023-04-03 23:09:42 +0200

    yuuu

--- Step1_4_TwoPrice.jl
+++ Step1_4_TwoPrice.jl
@@ -5,40 +5,62 @@ include("Data1.jl")
 
 
 #Model
-S1_TwoPrice = Model(Gurobi.Optimizer)
+S14_TwoPrice = Model(Gurobi.Optimizer)
 
-#Consider only the first 200 scenarios
+#Consider only 200 scenarios
 S = 200
 PI = [1/S for i=1:S]
 
+B = 1 #risk adversity (beta)
+A = 0.9 #Confidence level (alpha)
+
 #Variables
-@variable(S1_TwoPrice, w_da[h=1:H] >= 0) #day ahead hourly bidded production
-@variable(S1_TwoPrice, w_im[h=1:H,s=1:S])
-@variable(S1_TwoPrice, w_up[h=1:H,s=1:S] >= 0)
-@variable(S1_TwoPrice, w_dw[h=1:H,s=1:S] >= 0)
+@variable(S14_TwoPrice, w_da[h=1:H] >= 0) #day ahead hourly bidded production
+@variable(S14_TwoPrice, w_im[h=1:H,s=1:S])
+@variable(S14_TwoPrice, w_up[h=1:H,s=1:S] >= 0)
+@variable(S14_TwoPrice, w_dw[h=1:H,s=1:S] >= 0)
+
+@variable(S14_TwoPrice, n[s=1:S] >= 0) #auxilliary variable
+@variable(S14_TwoPrice, VAR) #value at risk
+
+@variable(S14_TwoPrice, EP[s=1:S]) #expected profit
+
 
 #Objective function One Price Scheme
-@objective(S1_TwoPrice, Max, 
+@objective(S14_TwoPrice, Max, 
             sum( sum(PI[s]
             * ((f_DA1[h,s] * w_da[h]) 
             + f_SB1[h,s] * ( (0.9 * f_DA1[h,s] * w_up[h,s]) - (f_DA1[h,s] * w_dw[h,s])  ) #active when the system has power excess
             + abs((f_SB1[h,s]-1)) * ( (f_DA1[h,s] * w_up[h,s]) - (1.2 * f_DA1[h,s] * w_dw[h,s]) ) #active when the system has power deficit
             ) 
             for s=1:S) for h=1:H)
+            + B * (VAR - 1/(1-A) * sum(PI[s] * n[s] for s=1:S)) #CVaR
             )
 
 #Constraints
-@constraint(S1_TwoPrice, [h=1:H], 0 <= w_da[h] <= W_Cap) #capacity constraint for the wind farm
-@constraint(S1_TwoPrice, [h=1:H,s=1:S], w_im[h,s] == f_WP1[h,s] - w_da[h] ) #real time imbalance
-@constraint(S1_TwoPrice, [h=1:H,s=1:S], w_im[h,s] == w_up[h,s] - w_dw[h,s] ) #excess and deficit imbalance equal the real time imbalance
+@constraint(S14_TwoPrice, [h=1:H], 0 <= w_da[h] <= W_Cap) #capacity constraint for the wind farm
+@constraint(S14_TwoPrice, [h=1:H,s=1:S], w_im[h,s] == f_WP1[h,s] - w_da[h] ) #real time imbalance
+@constraint(S14_TwoPrice, [h=1:H,s=1:S], w_im[h,s] == w_up[h,s] - w_dw[h,s] ) #excess and deficit imbalance equal the real time imbalance
 
+@constraint(S14_TwoPrice, [s=1:S], n[s] >= 0)
+@constraint(S14_TwoPrice, [s=1:S], VAR - EP[s] - n[s] <= 0)
+
+#not necessary for the model but used for the outputs
+@constraint(S14_TwoPrice, [s=1:S],  sum(((f_DA1[h,s] * w_da[h]) 
+                                    + f_SB1[h,s] * ( + (0.9 * f_DA1[h,s] * w_up[h,s]) - ( f_DA1[h,s] * w_dw[h,s]) ) 
+                                    + abs((f_SB1[h,s]-1)) * ( ( f_DA1[h,s] * w_up[h,s]) - (1.2 * f_DA1[h,s] * w_dw[h,s]) ) #active when the system has power deficit  
+                                    for h=1:H)) == EP[s])
 
 #Solve
-Solution = optimize!(S1_TwoPrice)
+Solution = optimize!(S14_TwoPrice)
 
+println("Risk adversity: $B")
+println("Expected Profit under the One Price scheme: $(round(sum(value.(EP[s])*PI[s] for s=1:S)))€")
+CVaR = value.(VAR - 1/(1-A) * sum(PI[s] * n[s] for s=1:S))
+println("CVaR: $(round(CVaR))€")
+println("Value at Risk: $(round(value.(VAR)))€")
 
-println("Profit under the Two Price scheme: $(round.(objective_value(S1_TwoPrice)))\$")
-
+#=
 #Outputs
 W_DA = value.(w_da[:])
 W_IM = value.(w_im[:,:])
@@ -65,3 +87,4 @@ for s=1:S
     println("$(round(f_WP1[h,s]))\t$(round(W_DA[h]))\t$(round(W_IM[h,s]))")
     end
 end
+=#
